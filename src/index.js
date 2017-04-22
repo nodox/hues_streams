5e466893a11ea2fbcbde5430f7f2c590744a3b64
nodox <snatera09@gmail.com>
2017-04-22 16:53:32 -0500

    added routing for reactjs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './routes';
+
+// import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Routes />,
   document.getElementById('root')
 );
