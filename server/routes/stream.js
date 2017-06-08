7c9606a3392dc7019b7b5c8c5b0004e0997c5690
nodox <snatera09@gmail.com>
2017-06-08 16:52:54 -0500

    feat: add more categories

--- server/routes/stream.js
+++ server/routes/stream.js
@@ -9,7 +9,7 @@ var streams = {
     id: 'be-inspired-get-motivated',
     name: 'Be Inspired, Get Motivated',
     description: 'Motivational videos on the internet streaming all day.',
-    tags: ['inspirational'],
+    tags: ['inspirational', 'motivational'],
     videos: []
   },
   '2-min-meals': {
@@ -23,33 +23,16 @@ var streams = {
     id: 'ball-is-life',
     name: '#BallisLife',
     description: 'Ball is life? Watch NBA updates all day long with these videos from the web.',
-    tags: ['sports'],
+    tags: ['nba'],
     videos: []
   }, 
-  'cute-animals': {
-    id: 'cute-animals',
-    name: 'Cute animals',
-    description: 'Watch the cutest animals on the web!',
-    tags: ['cute_animals'],
+  'ted-talks': {
+    id: 'ted-talks',
+    name: 'Ted Talks',
+    description: 'Watch thought provoking videos on the TED stage.',
+    tags: ['ted_talks'],
     videos: []
   },
-  'the-young-turks': {
-    id: 'the-young-turks',
-    name: 'The Young Turks',
-    description: 'A new form of internet journalism. Watch latest videos by The Young Turks',
-    tags: ['tyt'],
-    videos: []
-  },
-
-  'buzzfeed-videos-for-days': {
-    id: 'buzzfeed-videos-for-days',
-    name: 'Buzzfeed Videos For Days',
-    description: 'Trending Buzzfeed videos streaming to your device',
-    tags: ['entertainment'],
-    videos: []
-  },  
-
-
   'news-for-the-black-millennial': {
     id: 'news-for-the-black-millennial',
     name: 'News for the Black Millennial',
@@ -57,28 +40,77 @@ var streams = {
     tags: ['news'],
     videos: []
   },
+  'far-right-news': {
+    id: 'far-right-news',
+    name: 'Far-Right News',
+    description: 'The latest videos from far-right news outlets.',
+    tags: ['far_right_news'],
+    videos: []
+  },
 
-  'memes-memes-memes': {
-    id: 'memes-memes-memes',
-    name: 'Memes Memes Memes',
-    description: 'The Memes never stop. Watch all the trending memes on the internet. Facts B.',
+  'black_womens-fitness': {
+    id: 'black_womens-fitness',
+    name: 'Black Women Fitness',
+    description: 'Watch womens fitness videos from the web.',
+    tags: ['black_womens_fitness'],
+    videos: []
+  },
+  'political-satire': {
+    id: 'political-satire',
+    name: 'Political Satire',
+    description: 'Political satire: news in the truest form.',
+    tags: ['political_satire'],
+    videos: []
+  },
+  'cute-animals': {
+    id: 'cute-animals',
+    name: 'Cute animals',
+    description: 'Watch the cutest animals on the web!',
+    tags: ['cute_animals'],
+    videos: []
+  },
+  'buzzfeed-videos-for-days': {
+    id: 'buzzfeed-videos-for-days',
+    name: 'Buzzfeed Videos For Days',
+    description: 'Trending Buzzfeed videos streaming to your device',
+    tags: ['buzzfeed'],
+    videos: []
+  },  
+  'natural-haircare': {
+    id: 'natural-haircare',
+    name: 'Natural & Curly Haircare',
+    description: 'Watch the latest videos on how to care for your natural hair.',
+    tags: ['black_natural_hair'],
+    videos: []
+  },
+  'memes': {
+    id: 'memes',
+    name: 'Memes',
+    description: 'The Memes never stop. Watch all the trending memes on the internet.',
     tags: ['meme', 'funny'],
     videos: []
   },
-  // 'natural-and-curly-haircare': {
-  //   id: 'natural-and-curly-haircare',
-  //   name: 'Natural and Curly Haircare',
-  //   description: 'The latest videos on the web for dealing with curly hair.',
-  //   tags: ['haircare'],
-  //   videos: []
-  // }, 
-  // 'health-and-fitness-gurus': {
-  //   id: 'health-and-fitness-gurus',
-  //   name: 'Health and Fitness Gurus',
-  //   description: 'Learn from the best on how to get lean by working out.',
-  //   tags: ['workout'],
-  //   videos: []
-  // },
+  'spoken-word-poetry': {
+    id: 'spoken-word-poetry',
+    name: 'Spoken Word Poetry',
+    description: 'Watch spoken word poetry from around the web.',
+    tags: ['spoken_word'],
+    videos: []
+  },
+  'chess': {
+    id: 'chess',
+    name: 'Chess Videos',
+    description: 'Watch chess videos from the web.',
+    tags: ['chess'],
+    videos: []
+  },
+  'funny': {
+    id: 'funny',
+    name: 'Funny',
+    description: 'The funniest videos from all over the web.',
+    tags: ['funny'],
+    videos: []
+  },
 };
 
 
